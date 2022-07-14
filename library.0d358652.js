f6f4ebe04560ee5ced34c595e567e8fca5cf0894
Tasha1003Natasha <102981670+Tasha1003Natasha@users.noreply.github.com>
2022-07-14 11:20:49 +0000

    Deploying to gh-pages from @ Tasha1003Natasha/Filmoteka@6e8cdfd91d509351475e2008b62896c542cbba94 🚀

---
+++ library.0d358652.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".button-open"),o=document.querySelector(".backdrop-team"),c=document.querySelector(".modal-group__close-btn");e.addEventListener("click",(function(e){console.log(e),o.classList.remove("backdrop-hidden")})),c.addEventListener("click",(function(e){console.log(e),o.classList.add("backdrop-hidden")}));
+//# sourceMappingURL=library.0d358652.js.map
